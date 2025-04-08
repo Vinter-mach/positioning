fcc5e490bb4f485200b6f9d87e0fd292d2d77aab
Vinter-mach <vintermach@gmail.com>
2025-04-08 16:34:19 +0500

    third_task

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ closeButton.addEventListener('click', () => {
     modal.style.display = 'none';
     overlay.style.display = 'none';
 });
+
+document.querySelectorAll(".progressBar").forEach(progressbar => {
+    let progress = 0;
+    const intervalId = setInterval(() => {
+        if (progress < 100) {
+            progress++;
+            let progressLine = progressbar.firstElementChild;
+            progressLine.style.width = `${progress}%`;
+        } else {
+            clearInterval(intervalId);
+        }
+    }, 52);
+});
